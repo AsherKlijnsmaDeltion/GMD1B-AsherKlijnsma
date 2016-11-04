73163e6d9e38c548086438f62ad458eb681e4be9
AsherKlijnsmaDeltion <meneer.asher@live.nl>
2016-11-04 11:20:11 +0100

    project fix354

---
+++ P1/UnityProject/FlripperKost/Assets/Flipper2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flipper2 : MonoBehaviour
+{
+    public float flipperPower;
+    public float forcer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Q"))
+        {
+            
+
+        }
+    }
+}
---
+++ P1/UnityProject/FlripperKost/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour {
+
+	// Use this for initialization
+
+    void OnCollisionEnter(Collision C) {
+
+        Destroy(C.gameObject);
+        GameObject.Find("DataBase").GetComponent<GameController>().lives -= 1;
+        if(GameObject.Find("DataBase").GetComponent<GameController>().lives > 0){
+           GameObject.Find("SpawnLocation").GetComponent<Swapning>().SpawnBall();
+        }
+    }
+}
