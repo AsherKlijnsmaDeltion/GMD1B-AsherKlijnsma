14bc03f1904513c7cadef2d4f4314388090deaea
AsherKlijnsmaDeltion <meneer.asher@live.nl>
2017-06-23 22:55:26 +0200

    unity p2 herkansing

--- P2/UnityProject/scene/Assets/storestuff/Scripts/PickUp.cs
+++ P2/UnityProject/scene/Assets/storestuff/Scripts/PickUp.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PickUp : MonoBehaviour
 {
-    public RaycastHit hit;
+    
     public Vector3 ray;
     public Transform blade;
     public Transform handle;
@@ -20,24 +20,24 @@ public class PickUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        
+                
+                
+        }
+    
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.name == "blade")
         {
-            Debug.DrawRay(transform.position, transform.forward * 100, Color.blue, 5f);
-            if (Physics.Raycast(transform.position, transform.forward, out hit, range))
-            {
-                Debug.Log("Raycast hit; " + hit.transform.name);
-                if (hit.transform.name == "blade")
-                {
-                    GetComponent<Manager>().inventory[0] = true;
-                    Destroy(hit.transform.gameObject);
-                }
-                if(hit.transform.name == "handle")
-                {
-                    GetComponent<Manager>().inventory[1] = true;
-                    Destroy(hit.transform.gameObject);
-                }
-            }
+            GetComponent<Manager>().inventory[0] = true;
+            Destroy(col.transform.gameObject);
+        }
+        if (col.transform.name == "handle")
+        {
+            GetComponent<Manager>().inventory[1] = true;
+            Destroy(col.transform.gameObject);
         }
     }
 }
+
 
